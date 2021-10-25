f2c2108eb3583436eff07b64c97e131c4412b41e
Lelik Jan <80705108+Lelik7777@users.noreply.github.com>
2021-10-25 10:30:59 +0300

    add todoList for me under lesson2

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,48 @@
+import React from 'react';
 import './App.css';
-import {TodoList} from "./TodoList";
+import {TodoList} from './TodoList';
+import {useState} from 'react';
 
 
-export type TaskType={
-    id:number;
+export type TaskType = {
+    id: number;
+    title: string;
+    isDone: boolean;
 }
+export type FilterType = 'all' | 'active' | 'completed';
+
 function App() {
+    let [tasks, setTasks] = useState<TaskType[]>([
+        {id: 1, title: 'Html&&css', isDone: true},
+        {id: 2, title: 'Js', isDone: true},
+        {id: 3, title: 'React', isDone: false},
+        {id: 4, title: 'Angular', isDone: false},
+    ]);
+    let [filter, setFilter] = useState<FilterType>('all');
+    const getFilterTasks = (filer: FilterType) => {
+        switch (filer) {
+            case 'active':
+                return tasks.filter(x => !x.isDone);
+            case 'completed':
+                return tasks.filter(x => x.isDone);
+            default:
+                return tasks;
+        }
+    }
+    const removeTask = (id: number) => {
+        setTasks(tasks.filter(x => x.id != id));
+    }
+    const getStatusTasks = (filter:FilterType) => {
+      setFilter(filter);
+    }
     return (
         <div className="App">
-            <TodoList/>
+            <TodoList
+                data={getFilterTasks(filter)}
+                title={'List of technologies'}
+                removeTask={removeTask}
+                getStatusTasks={getStatusTasks}
+            />
         </div>
     );
 }
--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,15 +1,48 @@
-export function TodoList() {
+import React from 'react';
+import {FilterType, TaskType} from './App';
+
+export type TodoListType = {
+    data: TaskType[];
+    title: string;
+    removeTask: (id: number) => void;
+    getStatusTasks: (f: FilterType) => void;
+}
+
+export function TodoList(props: TodoListType) {
+    const statusAll = () => {
+        props.getStatusTasks('all');
+    }
+    const statusActive = () => {
+        props.getStatusTasks('active');
+    }
+    const statusCompleted = () => {
+        props.getStatusTasks('completed');
+    }
     return (
         <div className={'todoList'}>
-            <h1></h1>
+            <h1>{props.title}</h1>
             <div>
-                <input type="text" placeholder={'enter text'} />
+                <input type="text" placeholder={'enter new text'}/>
                 <button>+</button>
             </div>
+            <ul>
+                {props.data.map(x => {
+                    const removeTaskByBut = () => {
+                        props.removeTask(x.id);
+                    }
+                    return (
+                        <li>
+                            <input type="checkbox" checked={x.isDone}/>
+                            <span>{x.title}</span>
+                            <button onClick={removeTaskByBut}>x</button>
+                        </li>
+                    )
+                })}
+            </ul>
             <div>
-                <button>All</button>
-                <button>Active</button>
-                <button>Completed</button>
+                <button onClick={statusAll}>All</button>
+                <button onClick={statusActive}>Active</button>
+                <button onClick={statusCompleted}>Completed</button>
             </div>
         </div>
     )
