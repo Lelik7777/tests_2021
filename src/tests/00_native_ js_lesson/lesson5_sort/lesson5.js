96794a5b4edcd2b665474186b83e8f519937ac2a
Lelik Jan <80705108+Lelik7777@users.noreply.github.com>
2021-11-11 22:02:23 +0300

    add native js lesson5.js

--- src/tests/00_native_ js_lesson/lesson5_sort/lesson5.js
+++ src/tests/00_native_ js_lesson/lesson5_sort/lesson5.js
@@ -74,4 +74,7 @@ const students = [
         scores: 121
     },
 ];
-console.log(students.sort((a,b)=>{a.scores-b.scores}));
+/*console.log(students.sort((a,b)=>{a.scores-b.scores}));*/
+console.log(students)
+console.log(students.sort((a,b)=>a.name.toLowerCase()<=b.name.toLowerCase()?-1:1));
+console.log(students)
