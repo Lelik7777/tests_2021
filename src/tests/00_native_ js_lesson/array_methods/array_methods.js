f14b9f0504c1c52ba1590ef82ab76f10f05c7677
Lelik Jan <21alex7777@gmail.com>
2022-04-16 14:06:58 +0300

    in file array_methods.js  add some methods

--- src/tests/00_native_ js_lesson/array_methods/array_methods.js
+++ src/tests/00_native_ js_lesson/array_methods/array_methods.js
@@ -1,4 +1,6 @@
-//splice
+const red='color:red';
+const blue='color:blue';
+//splice change array has start, deleted quo, added items
 const array=[1,3,4,5,6,7,6]
 const removeArr=array.splice(2,0,'hello')
 console.log(array)
@@ -29,8 +31,42 @@ const string=array.join(' * ');
 console.log(string);
 console.log(typeof string);
 
-//Object.entries() return
+//Object.entries() return 2-D array
 const obj={name:'alex',age:44,friends:['bob','ann']};
 const arrayFromObj=Object.entries(obj)
 console.log(`%carray: %c${  Object.entries(arrayFromObj)}`,'color:red','color:blue');
 console.log(Object.entries(obj))
+
+//every return boolean check all items
+const isNumbers=array.every((x)=>typeof x==='number');
+console.log(isNumbers);
+const arrNum=[1,3,4,5,6,4];
+console.log(arrNum.every(x=>typeof x==='number'));
+
+//fill return new array(don`t change length) has start and end(not index but number of item)
+
+const newArrNum=arrNum.fill(2,1,2)
+console.log(newArrNum)
+
+//filter return new array of items that have been checked
+const filterNum=array.filter(x=>typeof x==='number');
+console.log(`%carray: %c${array}`,'color:red','color:blue')
+console.log(`%cfilterNum: %c${filterNum}`,red,blue);
+//copy
+console.log(array.filter(x=>x)==array)
+
+
+//find return first element or undefined (check incomplete)
+console.log(arrNum.find(x=>x===4));
+console.log(array.find(x=>typeof x==='string'));
+
+//forEach return undefined
+array.forEach(x=>x*x);
+console.log(array.forEach(x=>x-3));
+console.log(array.forEach(x => console.log('hello')));
+
+//Array.from  return array from array like or iterated object(Map or Set)
+console.log(Array.from('hello world'))
+const setItems=new Set(['dog','cat',window]);
+console.log( typeof setItems)
+console.log(Array.from(setItems));
