50dc2124ca7502857d1664cd6f5e8101df8d5cc5
Lelik Jan <21alex7777@gmail.com>
2022-04-20 11:41:39 +0300

    create draft02.js

---
+++ src/draft00/draft02/draft02.js
@@ -0,0 +1,9 @@
+const checkSimpleNum = (num) => {
+    let isSimple = true;
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) isSimple = false;
+    }
+    return isSimple;
+}
+console.log(checkSimpleNum(23));
+
