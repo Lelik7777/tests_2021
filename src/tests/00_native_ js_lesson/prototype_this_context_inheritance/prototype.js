63dd6183458379d0c55d2f459bfc82479d5f4491
Lelik Jan <21alex7777@gmail.com>
2022-05-04 17:28:07 +0300

    prototype syntax es5

--- src/tests/00_native_ js_lesson/prototype_this_context_inheritance/prototype.js
+++ src/tests/00_native_ js_lesson/prototype_this_context_inheritance/prototype.js
@@ -1,10 +1,36 @@
 const Animal=function (prop) {
     this.name=prop.name;
     this.age=prop.age;
-    this.barking=function () {
-        console.log('barking')
+    this.hasVoice=function () {
+        console.log('base voice from',this.name);
     }
 }
-const dog=new Animal({name:'boby',age:5});
+const dog=new Animal({name:'palkan',age:5});
 console.dir(dog);
-Animal.prototype.move=()=>{};
+Animal.prototype.move=()=>{
+    console.log('entity can move ')};
+console.log(Animal.prototype);
+console.log( dog.move())
+const Cat=function (data) {
+
+    Animal.apply(this,arguments);
+    this.hasTail=data.hasTail;
+    this.type=data.type;
+}
+//переопределяем прототип класса Cat
+Cat.prototype=Object.create(Animal.prototype);
+Cat.prototype.constructor=Cat;
+const cat=new Cat({name:'pushok',age:2,hasTail:true,type:'cat'});
+//если метод опрделен в прототипе, то мы можем его переопределить
+Animal.prototype.move=function () {
+    console.log(this.name+' move to');
+}
+Cat.prototype.move=function () {
+    console.log('cat '+this.name+' go to')
+}
+dog.move();
+console.log(cat);
+console.log(cat.hasVoice())
+cat.move()
+
+
