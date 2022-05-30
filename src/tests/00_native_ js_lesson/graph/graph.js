300b7a04396a4006a505bcd9d09ddb6dad55d9e9
Lelik Jan <21alex7777@gmail.com>
2022-05-30 13:27:15 +0300

    feat: add graph.js

---
+++ src/tests/00_native_ js_lesson/graph/graph.js
@@ -0,0 +1,36 @@
+const friendsList={
+    1:{
+        id:1,
+        name:'Bob',
+        gender:'male',
+        friends:[2,3,4]
+    },
+    2:{
+        id:2,
+        name:'Nick',
+        gender:'male',
+        friends:[1,3]
+    },
+    3:{
+        id:3,
+        name:'Piter',
+        gender:'male',
+        friends:[4,1,2]
+    },
+    4:{
+        id:4,
+        name:'Ann',
+        gender:'female',
+        friends:[1,3]
+    }
+}
+const findPerson = (list,id) => Object.values(list).find(x=>x.id===id);
+const personFriends=(list,id)=>{
+   const person=findPerson(list,id);
+return person.friends.map(x=>list[x])
+}
+console.log('Piter',friendsList[3])
+console.log('piter`s friends:',friendsList[3].friends.map(x=>friendsList[x]));
+console.log(Object.values(friendsList).find(x=>x.id==3))
+console.log(findPerson(friendsList,4))
+console.log(`${friendsList[4].name}'s friends: o`,personFriends(friendsList,4))
