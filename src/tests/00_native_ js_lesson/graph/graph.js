c0fb3b6d1e98b56fde8638441ac1b62fbebb0988
Lelik Jan <21alex7777@gmail.com>
2022-05-30 23:01:28 +0300

    feat: add graph.css

--- src/tests/00_native_ js_lesson/graph/graph.js
+++ src/tests/00_native_ js_lesson/graph/graph.js
@@ -33,4 +33,20 @@ console.log('Piter',friendsList[3])
 console.log('piter`s friends:',friendsList[3].friends.map(x=>friendsList[x]));
 console.log(Object.values(friendsList).find(x=>x.id==3))
 console.log(findPerson(friendsList,4))
-console.log(`${friendsList[4].name}'s friends: o`,personFriends(friendsList,4))
+console.log(`${friendsList[4].name}'s friends: o`,personFriends(friendsList,4));
+
+class Graph{
+    constructor(list) {
+    this.list=list;
+    }
+    getList(){
+        return this.list;
+    }
+    addVertex(vertex){
+        this.list[vertex.id]=vertex;
+    }
+}
+const currentList=new Graph(friendsList);
+console.log(currentList.getList());
+console.log(currentList.addVertex({id:5,name:'Tom',gender:'male',friends:[]}));
+console.log(currentList.getList());
