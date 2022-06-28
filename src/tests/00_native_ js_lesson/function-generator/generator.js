0f7c6c55405b1576c98cd0e12d239b385a12a58e
Lelik Jan <21alex7777@gmail.com>
2022-06-28 21:58:27 +0300

    feat: create own iterable object

--- src/tests/00_native_ js_lesson/function-generator/generator.js
+++ src/tests/00_native_ js_lesson/function-generator/generator.js
@@ -1,11 +1,12 @@
 //ф-ция генератор,когда вызвана, возвращает специальный объект "генератор"
-function* generator(){
+function* generator() {
     yield 1;
     yield 2;
     return 3;
 }
+
 // "функция-генератор" создаёт объект "генератор"
-let a=generator();
+let a = generator();
 
 console.log(a)
 console.log(a.next())
@@ -14,28 +15,29 @@ console.log(a.next())
 console.log(a.next())
 //генераторы - это итерируемая сущность,поэтому для перебора можно использовать for of , только нужно использовать
 // для возвращения только yield, поскольку return for of не увидит
-function* generator2(){
+function* generator2() {
     yield 1;
     yield 2;
     yield 3;
 }
-const objGenerator2=generator2();
-for (let value of objGenerator2){
+
+const objGenerator2 = generator2();
+for (let value of objGenerator2) {
     console.log(value)
 }
 //также можно использовать spread operator ... , но развернуть я могу лишь вызвов генератора, но не объект,который
 //возращает вызванный генератор
 
-console.log([0,...generator2()]);
-const arr=[1,3,34];
+console.log([0, ...generator2()]);
+const arr = [1, 3, 34];
 console.log(Math.max(...arr));
 console.log([...'hello'])
-const[prev,cur]=['предыдущий','текущий'];
+const [prev, cur] = ['предыдущий', 'текущий'];
 console.log(`prev=${prev} and cur=${cur}`)
 
 function* fibonacci() { // функция-генератор
     let [prev, curr] = [0, 1];
-    for (;;) {
+    for (; ;) {
         [prev, curr] = [curr, prev + curr];
         yield curr;
     }
@@ -47,12 +49,12 @@ for (let n of fibonacci()) {
         break;
     console.log(n);
 }
-for (let n of fibonacci()){
-    if(n>10) break;
+for (let n of fibonacci()) {
+    if (n > 10) break;
     console.log(n)
 }
 console.log('**********************************')
-var gen = (function *(){
+var gen = (function* () {
     yield 1;
     yield 2;
     yield 3;
@@ -66,4 +68,31 @@ for (let o of gen) {
 for (let o of gen) {
     console.log(o); // Не будет вызван
 }
+let [a0, b0, c0, d0] = [3, 4, 5, 6];
+console.log(a0, b0, c0, d0)
+
+// итерируемый объект
+
+class IterableObject extends Object {
+    constructor(obj) {
+        super();
+        Object.assign(this, obj);
+    }
+
+    [Symbol.iterator]() {
+        return {
+            next() {
+                return {
+                    value: undefined,
+                    done: false,
+                }
+            }
+        }
+    }
+}
+
+const iterableObj = new IterableObject({1: 'a', 2: 'b', 3: 'c'});
+console.log(iterableObj[Symbol.iterator]().next())
+
+
 
