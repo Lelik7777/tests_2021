2d614720c0502892c706207661a0e0dd50a311d7
Lelik Jan <21alex7777@gmail.com>
2022-08-22 18:11:33 +0300

    feat:add in draft02.js some information about weakMap,array

--- src/draft00/draft02/draft02.js
+++ src/draft00/draft02/draft02.js
@@ -1,3 +1,5 @@
+
+
 const checkSimpleNum = (num) => {
     let isSimple = true;
     for (let i = 2; i < num; i++) {
@@ -49,25 +51,67 @@ let urls2 = [
     'https://no-such-url'
     // new Error('ошибочный url')
 ];
+let bob={name:'bob'};
+const tom={name:'tom'};
+const ann={name:'ann'};
+const nick={name:'nick'};
+const arrUsers=[bob,tom,ann,nick]
+const text=`it is `;
+const siteData=new WeakMap();
+for (const el of arrUsers) {
+    siteData.set(el,text+el.name);
+}
+
+bob=null;
+//после этого bob и связанные с этим объектом данные автоматически удаляться
+console.log(siteData)
 console.log(new Map(Object.entries(userData)));
 for (const key in userData) {
     console.log(userData[key])
 }
+
 async function f() {
-const res=new Promise(res=>res('done'));
-return res;
+    const res = new Promise(res => res('done'));
+    return res;
 }
 
 console.log(f());
-f().then(res=>console.log(res));
+f().then(res => console.log(res));
 
-new Promise(function(resolve, reject) {
+new Promise(function (resolve, reject) {
     setTimeout(() => {
         throw new Error("Whoops!");
-    }, 1000); })
+    }, 1000);
+})
     .catch(alert);
-const a=userData?.age;
+const a = userData?.age;
 //по итогу выкинет не ошибку,а вернет undefined
-const b=userData?.job?.place?.office;
+const b = userData?.job?.place?.office;
 console.log(a);
 console.log(b);
+new Promise((res, rej) => {
+    throw new Error('error in 79 line')
+    // setTimeout(()=>{
+    //     throw new Error('error in line 80')
+    // },1000);
+}).then(res => console.log(res)).catch(er => console.log('error has done: ',er));
+console.log((async function () {
+   return  await new Promise(res=>res(' async function has done'));
+})())
+delete fruits[0];
+console.log(fruits.length);
+console.log(fruits)//['Апельсин', 'Груша', 'абрикос']
+console.log(fruits[0]);
+fruits.length=19;
+console.log(fruits)
+console.log(fruits[18]);
+console.log(fruits[120]);
+//counter for site
+const visitedSite=new WeakMap();
+function checkUsers(user) {
+    let count=visitedSite.get(user)||0;
+    visitedSite.set(user,count+1);
+}
+checkUsers(ann);
+checkUsers(tom);
+console.log(visitedSite)
