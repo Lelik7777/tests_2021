405f4d6b4f03b50d3860f04657ea400a153b3b93
Lelik Jan <21alex7777@gmail.com>
2022-08-29 13:31:09 +0300

    feat: add in draft02.js info about callback(error,data),complex calculation,await

--- src/draft00/draft02/app.js
+++ src/draft00/draft02/app.js
@@ -1,10 +1,13 @@
 const fs = require("fs");
-fs.appendFile('hello.txt','\nadd new text to hello.txt '+new Date(),(err => {
-    if(err) throw err;
+fs.appendFile('hello.txt', '\nadd new text to hello.txt ' + new Date(), (err => {
+    if (err) throw err;
     else {
-        fs.readFile('hello.txt','utf8',((err,data)=>{
-            if(err) throw err;
+        fs.readFile('hello.txt', 'utf8', ((err, data) => {
+            if (err) throw err;
             console.log(data);
         }))
     }
+}));
+fs.writeFile('text.txt', 'it is new text file which i have created now', (err => {
+    if (err) throw err;
 }))
--- src/draft00/draft02/draft02.js
+++ src/draft00/draft02/draft02.js
@@ -1,3 +1,5 @@
+
+
 console.log('%cdraft02', 'color:green;font-size:16px;margin-left:200px;')
 const userAny = {
     head: true,
@@ -33,27 +35,47 @@ let nick = {name: 'nick'};
 const arrUsers = [bob, tom, ann, nick];
 
 
-setTimeout((a, b) => {
-    console.log(a + b);
-    return a + b;
-}, 1000, 4, 5);
 
-function sum(a, b) {
-    console.log(a + b);
-    return a + b;
+console.log(255..toString(16));
+
+function loadImg(src,callback) {
+const img=document.createElement('img');
+img.src=src;
+img.onload=()=>{
+    callback(null,img);
+}
+img.onerror=()=>callback(new Error('error by loading image'));
+document.body.prepend(img);
+}
+function promiseLoad(src) {
+    return new Promise((res,rej)=>{
+        loadImg(src,(err,img)=>{
+            if(err) rej(err);
+            res(img);
+        })
+    })
 };
-setTimeout(sum, 2000, 23, 5);
-setTimeout('sum(4,44)', 3000);
+promiseLoad('https://picsum.photos/400').then(res=>console.log(res.src)).catch(er=>console.log(er));
+Object.seal(userData);
 
-const objGenerator = {
-    from: 1,
-    to: 4,
-    * [Symbol.iterator]() {
-        for (let i=this.from;i<=this.to;i++){
-            yield i;
-        }
-    }
+userData.job='programmer';
+delete userData.age;
+userData.age=55;
+console.log(userData);
+console.log(parseInt('100px'));
+const [a,b,c,d]=[3,4,true,'hello'];
+console.log(a,b,c,d);
+function calculationComplete() {
+    const start=Date.now();
+    for (let i=0;i<1e10;i++){}
+    console.log('time spent :',Date.now()-start,'ms');
 }
-for (const number of objGenerator) {
-    console.log(number);
+calculationComplete();
+async function funSome() {
+    const promise=new Promise(res=>{
+        setTimeout(res,3000,'done');
+    });
+   let res= await promise;
+    console.log(res);
 }
+funSome()
