41fb77318469adf657a463ad7409d8e794566e68
Lelik Jan <21alex7777@gmail.com>
2022-08-31 11:28:39 +0300

    feat: add in draft02.js info about WeakMap: cache obj and count visits of cite,function constructor

--- src/draft00/draft02/draft02.js
+++ src/draft00/draft02/draft02.js
@@ -32,27 +32,52 @@ let ann = {name: 'ann'};
 let nick = {name: 'nick'};
 const arrUsers = [bob, tom, ann, nick];
 
-const factorialNum = (num) => {
-    return num === 1 ? 1 : num * factorialNum(num - 1);
-};
-console.log(factorialNum(5));
-//неявный try...catch
-new Promise((res, rej) => {
-    setTimeout(() => {
-        throw new Error('error');
-        //rej(new Error('error'))
-    }, 0)
-}).catch(er => console.log(er));
-
-const powNum = (num, degree) => {
-    return degree === 1 ? num : num * powNum(num, degree - 1);
-}
-console.log(powNum(2, 3));
-const powNumCicle = (num, degree) => {
-    let res = 1;
-    for (let i = 0; i < degree; i++) {
-        res *= num;
+const range = (start, end) => {
+    if (end - start <= 1) return;
+    if (end - start === 2) return [start + 1];
+    else {
+        const array = range(start, end - 1);
+        array.push(end - 1);
+        return array;
     }
-    return res;
 }
-console.log(powNumCicle(2, 3));
+console.log(range(3, 2));
+console.log(+(0.2 + 0.1).toFixed(3) == 0.3);
+
+function User(name) {
+    this.name = name;
+    console.log(new.target);
+}
+
+const user = User('bob');
+const cache = new WeakMap();
+const process = (obj, cache) => {
+    const start = Date.now();
+    if (!cache.has(obj)) {
+        for (let i = 0; i < 1e9; i++) {
+        }
+        cache.set(obj, 'some  results of calculations');
+    }
+    return {
+        value: cache.get(obj),
+        'working time': Date.now() - start > 0 ? Date.now() - start : 'obj is in cache',
+    }
+}
+console.log(process(userData, cache));
+console.log(process(userData, cache));
+console.log(process(userData, cache));
+
+const dataVisited = new WeakMap();
+const countVisitsUser = (user, collection) => {
+    let count = collection.get(user) || 0;
+    collection.set(user, ++count);
+}
+countVisitsUser(ann,dataVisited);
+countVisitsUser(ann,dataVisited);
+countVisitsUser(nick,dataVisited);
+countVisitsUser(ann,dataVisited);
+countVisitsUser(bob,dataVisited);
+countVisitsUser(bob,dataVisited);
+console.log(dataVisited);
+bob=null;
+console.log(dataVisited.has(bob));
