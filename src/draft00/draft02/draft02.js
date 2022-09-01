0df9ddbb0b258faeee9547a978994b07ea664587
Lelik Jan <21alex7777@gmail.com>
2022-09-01 11:49:16 +0300

    feat: add in draft02.js function isPrimes

--- src/draft00/draft02/draft02.js
+++ src/draft00/draft02/draft02.js
@@ -32,52 +32,20 @@ let ann = {name: 'ann'};
 let nick = {name: 'nick'};
 const arrUsers = [bob, tom, ann, nick];
 
-const range = (start, end) => {
-    if (end - start <= 1) return;
-    if (end - start === 2) return [start + 1];
-    else {
-        const array = range(start, end - 1);
-        array.push(end - 1);
-        return array;
-    }
-}
-console.log(range(3, 2));
-console.log(+(0.2 + 0.1).toFixed(3) == 0.3);
+const arr = new Array(7);
+console.log(arr);
+console.log(Array.of(7));
 
-function User(name) {
-    this.name = name;
-    console.log(new.target);
+const isPrime = (num) => {
+  for (let i=2;i<num;i++){
+      if(num%i===0) return false;
+  }
+  return true;
 }
-
-const user = User('bob');
-const cache = new WeakMap();
-const process = (obj, cache) => {
-    const start = Date.now();
-    if (!cache.has(obj)) {
-        for (let i = 0; i < 1e9; i++) {
-        }
-        cache.set(obj, 'some  results of calculations');
-    }
-    return {
-        value: cache.get(obj),
-        'working time': Date.now() - start > 0 ? Date.now() - start : 'obj is in cache',
-    }
+const isPrimes = (num) => {
+  for (let i=2;i<=num;i++){
+      if(!isPrime(i))continue;
+      console.log(i);
+  }
 }
-console.log(process(userData, cache));
-console.log(process(userData, cache));
-console.log(process(userData, cache));
-
-const dataVisited = new WeakMap();
-const countVisitsUser = (user, collection) => {
-    let count = collection.get(user) || 0;
-    collection.set(user, ++count);
-}
-countVisitsUser(ann,dataVisited);
-countVisitsUser(ann,dataVisited);
-countVisitsUser(nick,dataVisited);
-countVisitsUser(ann,dataVisited);
-countVisitsUser(bob,dataVisited);
-countVisitsUser(bob,dataVisited);
-console.log(dataVisited);
-bob=null;
-console.log(dataVisited.has(bob));
+isPrimes(44)
