3113bfd65e891befb1204ee8e403085daecd3a0e
Lelik Jan <21alex7777@gmail.com>
2022-09-21 13:51:16 +0300

    feat: add in draft02.js add information about typeof, descriptor

--- src/draft00/draft02/draft02.js
+++ src/draft00/draft02/draft02.js
@@ -37,47 +37,12 @@ let ann = {name: 'ann'};
 let nick = {name: 'nick'};
 const arrUsers = [bob, tom, ann, nick];
 
-console.log(userData.__proto__.head)
+console.log(typeof 30n);
 
-class User {
-    constructor(name) {
-        this.name = name;
-    }
-}
-
-const user1 = new User('bob');
-console.log(user1);
-user1.__proto__ = {age: 30};
-console.log(user1);
-user1.__proto__.getYearBorn = function () {
-    return new Date().getFullYear() - this.age;
-}
-console.log(user1);
-console.log(user1.__proto__.getYearBorn());
-const a = 5.45;
-console.log(a);
-console.log(+a.toFixed());
-const b=new Number();
-console.log(b)
-const str=new String();
-console.log(str.valueOf());
-console.log(b.valueOf())
-console.log(userData.valueOf());
-console.log(''.valueOf())
-const c=3;
-console.log(c.valueOf());
-const _true=true;
-console.log(_true.valueOf());
-const someObj={name:'bob',age:33};
-console.log(someObj.valueOf());
-
-console.log(.25*345);
-console.log(Object.is(3,3));
-console.log(userData.hasOwnProperty('age'));
-console.log(Object.getOwnPropertyDescriptor(userData,'firstName'));
+console.log(Object.getOwnPropertyDescriptor(userData,'age'));
 console.log(1e4);
 console.log(1e-4);
-
+console.log(arrUsers.valueOf())
 
 
 
