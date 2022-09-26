5c82f54c43bdd4c901196ff03ba465b75786ce2c
Lelik Jan <21alex7777@gmail.com>
2022-09-26 10:12:55 +0300

    feat: add in draft02.js add information about function constructor, method call(), promise, this in regular function,var damage, Map(),

--- src/draft00/draft02/draft02.js
+++ src/draft00/draft02/draft02.js
@@ -36,16 +36,87 @@ let tom = {name: 'tom'};
 let ann = {name: 'ann'};
 let nick = {name: 'nick'};
 const arrUsers = [bob, tom, ann, nick];
+const copy = {...userData};
+console.log(copy === userData);
+console.log('copy', copy);
+console.log(userData.getYeaBorn?.() ?? 'not exist');
+console.table(userData);
 
-const nestedArr = [3, 4, [5, 6]];
-const [a, , [c, d]] = nestedArr;
-console.log(a, c, d);
-const fun = ({age = 33, name = 'bob', job = 'admin'}) => {
-    console.log(`it is ${name}, age is ${age}, work as ${job}`);
+function* gen(start, end) {
+    for (let i = start; i < end; i++) yield i;
 }
-fun({name: 'tom', job: 'developer', location: {city: 'Moscow'}});
-console.log(userData.getName?.() ?? 'not exist');
-console.log(userData);
-console.log(Object.defineProperties(userData, {job: {value: 'developer'}, age: {value: 22}}))
-console.log(userData);
-console.log(Object.entries(userData));
+
+function* genPass() {
+    yield* gen(50, 70);
+    yield* gen(71 - 109);
+}
+
+const map = new Map(Object.entries(userData));
+console.log(map);
+console.log(map.entries());
+console.log(map.keys());
+console.log(map.values());
+for (const el of map.values()) {
+    console.log(el);
+}
+console.log(userData.valueOf());
+console.log(arrUsers.valueOf());
+const f = 44;
+console.log(f.valueOf());
+console.log(2e3);
+console.log(1e-2);
+
+if (!countProd) deleteProd();
+var countProd = 19;
+
+function deleteProd() {
+    console.log('all products delete');
+}
+
+function f1(name, age) {
+    return {name, age};
+}
+
+const bob1 = f1('bob', 88);
+console.log(bob1);
+const prom1 = new Promise((res, rej) => res('one'));
+const prom2 = new Promise((res, rej) => res('two'));
+prom1.then(res => {
+    console.log(res);
+    return prom2;
+}).then(res => console.log(res));
+console.log(Object.getOwnPropertyDescriptor(userData,{}));
+const tom11 = {
+    name: 'tom',
+    year: 2000,
+    getAge() {
+        console.log(new Date().getFullYear() - this.year);
+        function isUSSR() {
+            console.log(this.year<1993);
+            console.log(this);
+        }
+        isUSSR.call(this);
+    }
+}
+
+tom11.getAge();
+function f2(...rest) {
+    console.log(rest);
+    let sum=0;
+    for (const el of rest) {
+        sum+=el;
+    }
+    return sum;
+}
+
+console.log(f2(3,4,5,6));
+function User(name) {
+    this.name=name;
+}
+const userBob=User('bob');
+console.log(userBob);
+function f3() {
+    console.log(this);
+
+}
+f3();
