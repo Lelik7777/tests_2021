6709193cbff1f021030466738c68a7b33b98c6b3
Lelik Jan <21alex7777@gmail.com>
2023-04-02 12:06:30 +0300

    feat: add solution for task /Duplicate Encoder/

---
+++ src/2023/codewars/codewars.js
@@ -0,0 +1,7 @@
+//Duplicate Encoder
+/*The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.*/
+const duplicateEncode = (word) => [...word.toLowerCase()].map((char, i, arr) => arr.filter(el => arr[i] === el).length > 1 ? ')' : '(').join('');
+
+
+
+
--- src/2023/test scopes and functions/script.js
+++ src/2023/test scopes and functions/script.js
@@ -23,37 +23,67 @@ var user = {
     }
 };
 var printMessage = user.printMessage;
+
 //printMessage();
 
-function sum(a,b,c) {
+function sum(a, b, c) {
     console.log(arguments);
-    return a,b,c;
+    return a, b, c;
 }
-sum(1,3,4);
+
+sum(1, 3, 4);
 
 function getThis() {
     return this;
 }
 
 console.log(getThis());
-for (let i = 0; i <3 ; i++) {
+for (let i = 0; i < 3; i++) {
     setTimeout(function () {
         console.log(i);
-    },1000);
+    }, 1000);
 }
-const details={
-    message:'hello',
+const details = {
+    message: 'hello',
 }
+
 function getMessage() {
     return this.message;
 }
 
 console.log(getMessage.call(details));
-let name1='John';
+let name1 = 'John';
+
 function getName() {
     console.log(name);
 }
-setTimeout(()=>{
-    let namae1='Peter';
+
+setTimeout(() => {
+    let namae1 = 'Peter';
     getName();
-},1000);
+}, 1000);
+
+//0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144.
+function nthFibo(n) {
+    return n === 1 ? 0 : n === 2 ? 1 : nthFibo(n - 2) + nthFibo(n - 1);
+}
+
+console.log(nthFibo(7));
+const trans = (string) => {
+    return [...string.toLowerCase()].map((x, i, arr) => {
+
+        if (arr.filter(x => arr[i] === x).length > 1) {
+            return ')';
+        } else {
+            return '(';
+        }
+    }).join('');
+}
+//")())())"
+
+const duplicateEncode = (word) => [...word.toLowerCase()].map((char, i, arr) => arr.filter(el => arr[i]===el).length > 1 ? ')' : '(').join('');
+console.log(duplicateEncode("Success"));
+console.log('HeLlo'.replace(/[A-Z]/g, ''));
+console.log([...'Hello'.replace(/[A-Z]/g, '')].filter(z => z !== ''))
+console.log(new Date(2))
+console.log(3.145e7);
